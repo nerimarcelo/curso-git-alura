a102ff659f6dd35151f04abfc626cb3dc9876324
Marcelo <neri@neri>
2022-12-25 10:20:12 -0300

    START: Commit inicial

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {                                    # Qual é o cloud provider, pode ser GCP ou Azure ou AWS. Não oficiais são mantidos pela comunidade
+  region  = "sa-east-1"                             # Qual é a região que a instância vai ser provisionada
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0b22b708611ed2690"           # Nome da imagem. Esta é a imagem para Ubuntu 22.04
+  instance_type = "t2.micro"                        # Tipo da instância, o t2.micro é tier free
+  key_name      = "aws-marcelo"                     # Associa um par de chaves ssh a instancia. Esse par tem que estar criado lá na AWS. Não precisa do formato .pem. Precisa estar na mesma pasta =/ 
+  
+  tags = {
+    Name = "alura-aws-ubuntu"                       # Nome da instância
+  }
+}
+ 
